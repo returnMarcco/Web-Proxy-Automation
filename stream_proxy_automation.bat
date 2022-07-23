c95cc96b1914b957c6723b04f99ed2d9fe1f9b52
returnMarcco <106223269+returnMarcco@users.noreply.github.com>
2022-07-23 16:58:54 +1000

    Update stream_proxy_automation.bat

--- stream_proxy_automation.bat
+++ stream_proxy_automation.bat
@@ -7,14 +7,14 @@ Rem Written by returnMarcco - Give me a follow on both YouTube and Twitch if thi
 
 REM Twitch - "https://"twitch.tv/returnMarcco" | Youtube (returnMarcco{ENTER}) - "https://www.youtube.com/channel/UC1aPOXPpMAKbPOT0Hw1GbMg"
 
-Rem Script Description: Automatically opens a specified number of Chrome browser tabs and connects to Twitch.tv/makingVelvet via Web-Proxy
+Rem Script Description: Automatically opens a specified number of Chrome browser tabs and connects to Tthe URL of your choice via Web-Proxy
 Rem Sources; Stackoverflow, https://www.codegrepper.com/code-examples/whatever/Make+a+batch+file+that+opens+site+in+browser+and+enter+login+information
 
 Rem *** README **** - In order to have this script functioning for your Twitch channel, you must follow these instructions;
 REM Instruction List
 REM For Windows OS only
 REM Step 1) If you are reading this, then you have opened the script in a text editor (notepad for example). This is the first step
-REM Step 2) Replace the Twitch URL below with YOUR Twitch channel's URL. DO NOT REMOVE the {ENTER} next to the URL. Format Example: %SendKeys% "https://twitch.tv/newLifePlus{ENTER}"
+REM Step 2) Replace the example URL below with any URL of your chosing. DO NOT REMOVE the {ENTER} next to the URL. Format Example: %SendKeys% "https://twitch.tv/newLifePlus{ENTER}"
 REM Step 3) Save and close the script file
 
 REM To Use - IMPORTANT: START YOUR LIVESTREAM BEFORE RUNNING THIS SCRIPT 
@@ -27,7 +27,7 @@ set SendKeys=CScript //nologo //E:JScript "%~F0"
 for /L %%I IN (1, 1, 5) DO (
     START CHROME "https://blockaway.net"
     timeout /t 6
-    %SendKeys% "https://twitch.tv/returnMarcco{ENTER}"
+    %SendKeys% "https://exampleurl.comENTER}"
 )
 
 goto :EOF
