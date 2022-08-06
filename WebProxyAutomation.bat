49fe6f195a5e354bb66e1ba502cca2ab5ae2d5e9
returnMarcco <106223269+returnMarcco@users.noreply.github.com>
2022-08-07 01:22:01 +1000

    Update WebProxyAutomation.bat

--- WebProxyAutomation.bat
+++ WebProxyAutomation.bat
@@ -1,19 +1,7 @@
 @if (@CodeSection == @Batch) @then
-
-
 @echo off
-Rem Only for Windows OS
-Rem Written By returnMarcco - Give me a follow on both YouTube and Twitch if this helped you :)
 
-REM Twitch - "https://"twitch.tv/returnMarcco" | Youtube - "https://www.youtube.com/channel/UC1aPOXPpMAKbPOT0Hw1GbMg"
-
-Rem Script Description: Prompts you for a destination URL, as well as the number of times you wish to connect to that URL.
-Rem Sources; Stackoverflow, https://www.codegrepper.com/code-examples/whatever/Make+a+batch+file+that+opens+site+in+browser+and+enter+login+information
-
-REM Instruction List
-REM For Windows OS only
-
-REM The Only Step) Follow the prompts. It's that simple.
+REM An automation script console application for navigating to a user-specified URL for a user-specified number of times. (Windows OS Only)
 
 set SendKeys=CScript //nologo //E:JScript "%~F0"
 
